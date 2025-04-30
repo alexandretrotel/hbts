1d9113c9dd652ce7bf586995287dde2458d70b0f
Alexandre Trotel <trotelalexandre@proton.me>
2025-05-01 00:42:19 +0200

    chore: bump version to 1.0.19 in package.json; fix typo in stoppedAt column name in habits.schema.ts

--- src/db/schema/habits.schema.ts
+++ src/db/schema/habits.schema.ts
@@ -3,6 +3,6 @@ import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 export const badHabits = pgTable('bad_habits', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: text('name').notNull(),
-  stoppedAt: timestamp('started_at').notNull(),
+  stoppedAt: timestamp('stopped_at').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
