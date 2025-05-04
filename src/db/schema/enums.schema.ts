f2324ba20de245a2455e7b9b2985167ffce750bd
Alexandre Trotel <trotelalexandre@proton.me>
2025-05-04 19:22:44 +0200

    feat: add frequency enum and update good habits schema to include frequency

---
+++ src/db/schema/enums.schema.ts
@@ -0,0 +1,9 @@
+import { pgEnum } from 'drizzle-orm/pg-core';
+
+export const frequencyEnum = pgEnum('frequency', [
+  'multiple_times_a_day',
+  'daily',
+  'weekly',
+  'monthly',
+  'yearly',
+]);
--- src/db/schema/habits.schema.ts
+++ src/db/schema/habits.schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  real,
+  text,
+  timestamp,
+  unique,
+  uuid,
+} from 'drizzle-orm/pg-core';
+import { frequencyEnum } from './enums.schema';
 
 export const badHabits = pgTable('bad_habits', {
   id: uuid('id').primaryKey().defaultRandom(),
@@ -6,3 +14,26 @@ export const badHabits = pgTable('bad_habits', {
   stoppedAt: timestamp('stopped_at').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+
+export const goodHabits = pgTable('good_habits', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  frequency: frequencyEnum('frequency').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+});
+
+export const goodHabitsDailyLog = pgTable(
+  'good_habits_daily_log',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    goodHabitId: uuid('good_habit_id')
+      .notNull()
+      .references(() => goodHabits.id, {
+        onDelete: 'cascade',
+      }),
+    date: timestamp('date').notNull(),
+    quantity: real('quantity'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (t) => [unique().on(t.goodHabitId, t.date)]
+);
